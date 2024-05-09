f57d49d35e69f676d58865a2eddd135d5e76a927
san8383 <san8383@gmail.com>
2024-05-09 16:57:58 +0300

    Add files via upload

---
+++ install_env.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+apt update && apt install alacritty && apt install tmux && git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+git clone https://github.com/san8383/tmux_with_alacritty.git && cd tmux_with_alacritty
+cp .zshrc ~/.zshrc
+cp .tmux.conf ~/.tmux.conf
+mkdir ~/.local/bin/ && cp alacritty-with-tmux.sh ~/.local/bin/alacritty-with-tmux.sh # make link on that script to run tmux with allacrity on mouse click
+cp alacritty.yml ~/.config/alacritty/alacritty.yml
